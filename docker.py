42a8fb7eb80fb3536a8bd0994c8db689a0252f13
iamitops <amitanandsingh1998@gmail.com>
2026-05-12 23:14:09 +0530

    Code and Dockerfile updated

--- docker.py
+++ docker.py
@@ -1,2 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 print("practice docker in change for branch")
 
+@app.route("/")
+def home():
+    return "practice docker in change for branch"
+
+app.run(host="0.0.0.0", port=8080)
